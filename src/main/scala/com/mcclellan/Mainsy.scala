21309a7ecef4c018abbb42cf7804fa5c364c947d
Matt McClellan <mcclellan.mj@gmail.com>
2013-04-23 16:52:24 -0500

    Added status return code to metadata, now reloads directory at every run so that changes can be made without restarting

--- src/main/scala/com/mcclellan/Mainsy.scala
+++ src/main/scala/com/mcclellan/Mainsy.scala
@@ -26,25 +26,29 @@ import spray.httpx.marshalling.BasicMarshallers
 import net.liftweb.json.parse
 import net.liftweb.json.DefaultFormats
 import spray.http.HttpHeader
+import spray.http.HttpResponse
+import net.liftweb.json.JObject
+import net.liftweb.json.JValue
+import java.util.Date
+import java.net.URLEncoder
 
 case class Header(val header : String, val value : String)
-case class Param(val key : String, val value : String)
-case class Metadata(val header : Option[Set[Header]], val queryParams : Map[String, String])
+case class Metadata(val status : Int, val header : Option[Set[Header]], val query : Map[String, String])
 
 case class GenericHttpHeader(val name : String, val value : String) extends HttpHeader {
-  def lowercaseName: String = name.toLowerCase()
+	lazy val lowercaseName = name.toLowerCase()
 }
 
 object MetadataFileDirectory {
 	private val Pattern = "(.*)\\.metadata$".r
-	
+
 	def findReturnName(metaFilename : String, folder : File) : String = metaFilename match {
 		case Pattern(c) => {
-			def isValid(file:File) = file.getName.startsWith(c) && !file.getName.endsWith(".metadata")
-			
+			def isValid(file : File) = file.getName.startsWith(c) && !file.getName.endsWith(".metadata")
+
 			val returnFiles = folder.listFiles.filter(isValid).map(_.getPath)
-			assert(returnFiles.size == 1, "Invalid number of files to return, found " + returnFiles + 
-					". Ensure each metadata file only has one associated file")
+			assert(returnFiles.size == 1, "Invalid number of files to return, found " + returnFiles +
+				". Ensure each metadata file only has one associated file")
 			returnFiles.head
 		}
 		case _ => throw new RuntimeException("Failed to find file for metadata file " + metaFilename)
@@ -53,56 +57,89 @@ object MetadataFileDirectory {
 
 class MetadataFileDirectory(val path : File) {
 	implicit val formats = DefaultFormats
-	
+
+	def toMetadata(json : JValue) = {
+		val headers = (json \ "headers").extractOpt[Set[Header]]
+		val status = (json \ "status").extractOpt[Int].getOrElse(200)
+		val queryParams = (json \ "query").extract[JObject].values
+
+		assert(!queryParams.isEmpty, "No query parameters for " + path.getPath())
+		Metadata(status, headers, queryParams.map(entry => (entry._1, entry._2.toString)).toMap)
+	}
+
+	def autoclose[T <: { def close() : Unit }, V](source : T)(f : T => V) : V = {
+		import scala.language.reflectiveCalls
+		try {
+			f(source)
+		} finally {
+			source.close
+		}
+	}
+
 	lazy val metadataFiles = path.listFiles().filter(_.getName().endsWith(".metadata")).map(file => {
 		(MetadataFileDirectory.findReturnName(file.getName(), path), {
-				val json = parse(Source.fromFile(file).getLines.mkString(""))
-				val headers = (json \ "headers").extractOpt[Set[Header]]
-				val queryParams = (json \ "query").extract[Set[Param]]
-				assert(!queryParams.isEmpty, "No query parameters for " + path.getPath())
-				Metadata(headers, queryParams.map(param => (param.key, param.value)).toMap)
-			})
+			autoclose(Source.fromFile(file)) {
+				source => toMetadata(parse(source.getLines.mkString("")))
+			}
+		})
 	}).toMap
 }
 
 class Service extends Actor with HttpService {
-	lazy val metadataMaps = new MetadataFileDirectory(new File("c:\\Development\\metadata")).metadataFiles
+	def metadataMaps = new MetadataFileDirectory(new File("c:\\Development\\metadata")).metadataFiles
+
 	val bigIntMarshaller = Marshaller.of[BigInt](ContentType.`text/plain`) { (value, contentType, ctx) =>
 		ctx.marshalTo(HttpBody(contentType, value.toString + " "))
 	}
-	
+
+	def byteArrayOf(path : String) = {
+		val bis = new BufferedInputStream(new FileInputStream(path))
+		Stream.continually(bis.read).takeWhile(_ != -1).map(_.toByte).toArray
+	}
+
 	def actorRefFactory = context
 
 	def receive = handleTimeouts orElse runRoute(
-		get {
-			path("ping") { ctx =>
-				val params = ctx.request.queryParams.toSet
-				// FIXME: Getting messy, this needs to be broken up
-				metadataMaps.find(entry => entry match {
-					case (key, Metadata(headers, query)) => {
-						val intersection = (query.toSet intersect params)
-						intersection.size == query.toSet.size && intersection.size == params.size
+		(get & path("mocked")) { ctx =>
+			val params = ctx.request.queryParams.toSet
+			val metadata = metadataMaps.find(entry => entry match {
+				case (key, Metadata(_, _, query)) => (query.toSet intersect params).size == query.size
+			})
+
+			metadata match {
+				case Some((path, entry)) => {
+					import spray.http.StatusCode.int2StatusCode
+					// TODO: Should eventually be a chunked stream response
+					val bytes = byteArrayOf(path)
+
+					val headers = entry match {
+						case Metadata(_, Some(headers), _) => headers.map(header => GenericHttpHeader(header.header, header.value)).toList
+						case _ => Nil
 					}
-				}) match {
-					case Some((key, entry)) => {
-						// TODO: Should eventually be a chunked stream response
-						val bis = new BufferedInputStream(new FileInputStream(key))
-						val bytes = Stream.continually(bis.read).takeWhile(_ != -1).map(_.toByte).toArray
-						
-						val headers = entry match {
-							case Metadata(Some(headers), _) => headers.map(header => GenericHttpHeader(header.header, header.value)).toList
-							case _ => Nil
-						}
-						ctx.complete(StatusCodes.OK, headers, bytes)
-					}
-					case None => {
-						// TODO: Allow a default response if its invalid
-						println("No mapping for " + params)
-						ctx.complete(StatusCodes.NotFound, Nil, "")
-					}
+
+					ctx.complete(entry.status, headers, bytes)
+				}
+				case None => {
+					// TODO: Allow a default response if its invalid
+					println("No mapping for " + params)
+					ctx.complete(StatusCodes.NotFound, Nil, "")
 				}
 			}
+		} ~ (get & path("list")) { ctx =>
+			ctx.complete(metadataMaps.map(entry => {
+				val (filename, metadata) = entry
+
+				val combinedParams = for (
+					entry <- metadata.query;
+					list = List(entry._1, entry._2)
+				) yield list.map(encode).mkString("=")
+
+				"http://" + ctx.request.hostAndPort + "/mocked?" + combinedParams.mkString("&") +
+					"\n\t" + filename
+			}).mkString("\n\n"))
 		})
+
+	def encode = URLEncoder.encode(_ : String, "UTF-8")
 
 	def handleTimeouts : Receive = {
 		case Timeout(x : HttpRequest) =>
